06cbbd5babceb199de4591573e66d61baf79ee39
listopadgeorgii <listopadiogl@gmail.com>
2022-11-17 23:50:39 +0300

    global variables and scopes

--- js_fcc_functions.js
+++ js_fcc_functions.js
@@ -1,22 +1,42 @@
+function hello () {
+    console.log("Another string");
+    return  "Hello functions";
+}
+
+console.log(hello());
+
 function reusableFunction () {
-    console.log('Hi world');
+    console.log("Hi World!")
 }
 
 reusableFunction();
 
-function functionWithArgs(a, b) {
-    console.log(a + b);
+function testFun(param1, param2) {
+    console.log(param1, param2);
 }
 
-functionWithArgs(1, 2);
-functionWithArgs(7, 9);
+testFun("Russian", "Federation");
+testFun("United", "States");
+testFun("Costa", "Rica");
 
-function timesFive(num) {
-    return num * 5;
+function functionWithArgs (a, b) {
+    console.log (a + b);
+};
+
+functionWithArgs(1,2);
+functionWithArgs(7,9);
+
+let nyc = "New York";
+function unitedStates () {
+    let nv = "Nevada";
+    let ca = "California";
+
+    let al = "Alaska";
+    var nameOfState = "United States of America"
+   // console.log(nyc)
+
+    return nameOfState
 }
 
-const answer = timesFive(5)
-
-
-
-
+//console.log(unitedStates());
+console.log(nyc)
