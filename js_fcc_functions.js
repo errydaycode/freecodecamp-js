abf07bdafd677b5d29caeb86f2ed57edd2055916
listopadgeorgii <listopadiogl@gmail.com>
2022-11-18 01:11:01 +0300

    Undefined Value returned from a Function

--- js_fcc_functions.js
+++ js_fcc_functions.js
@@ -58,3 +58,21 @@ function myOutfit() {
 
 console.log(myOutfit());
 console.log(outerWear);
+
+// Setup
+let sum = 0;
+
+function addThree() {
+    sum = sum + 3;
+}
+
+// Only change code below this line
+
+function addFive () {
+    sum += 5;
+}
+
+// Only change code above this line
+
+console.log(addThree());
+console.log(addFive());
