2898c367eb27f6d004728042bdf65c7ce7e62106
listopadgeorgii <listopadiogl@gmail.com>
2022-11-21 23:45:20 +0300

    some function

--- js.js
+++ js.js
@@ -181,3 +181,11 @@ const joinNumbersFromRange = (start, finish) => {
 
     return result;
 };
+
+const printReversedWordBySymbol = (word) => {
+    let i = word.length - 1;
+    while (i >= 0) {
+        console.log(word[i]);
+        i = i - 1;
+    }
+};
