39d04f8ba3e5dad7061a31f07742deb63e9d198b
listopadgeorgii <listopadiogl@gmail.com>
2022-11-26 19:23:39 +0300

    do while loop

--- js_fcc_functions.js
+++ js_fcc_functions.js
@@ -501,9 +501,14 @@ console.log(phoneticLookup("echo"));
 //     myArray.push(i);
 // }
 
-const myArr = [2, 3, 4, 5, 6];
-let total = 0;
-for (let i = 0; i < myArr.length; i++) {
-    total += myArr[i];
-}
 
+const myArray = [];
+let i = 10;
+
+
+do {
+    myArray.push(i);
+    i++;
+} while (i < 5);
+
+console.log(myArray)
