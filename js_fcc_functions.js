5121ae1d34a8b17ddb20c9c4fe910103f375434b
listopadgeorgii <listopadiogl@gmail.com>
2022-11-28 02:02:30 +0300

    profile lookup

--- js_fcc_functions.js
+++ js_fcc_functions.js
@@ -563,3 +563,47 @@ function moneyRecursion () {
 }
 
 moneyRecursion();
+
+
+// Setup
+const contacts = [
+    {
+        firstName: "Akira",
+        lastName: "Laine",
+        number: "0543236543",
+        likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+        firstName: "Harry",
+        lastName: "Potter",
+        number: "0994372684",
+        likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+        firstName: "Sherlock",
+        lastName: "Holmes",
+        number: "0487345643",
+        likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+        firstName: "Kristian",
+        lastName: "Vos",
+        number: "unknown",
+        likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+];
+// console.log(contacts[3]["firstName"])
+
+function lookUpProfile(name, prop) {
+
+    for (var i = 0; i < contacts.length; i++) {
+        if(contacts[i].firstName === name) {
+            return contacts[i][prop] || "No such property";
+        }
+    }
+    return "No such contact";
+}
+
+
+
+console.log(lookUpProfile("Harry", "lastName"));
