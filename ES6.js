b79bcca4de915fff2c95b210a76096330ad53fde
listopadgeorgii <listopadiogl@gmail.com>
2022-11-29 16:46:35 +0300

    Use Destructuring Assignment to Assign Variables from Nested Objects

--- ES6.js
+++ ES6.js
@@ -7,3 +7,14 @@ function checkScope() {
     console.log('Function scope i is: ', i);
     return i;
 }
+
+const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+};
+
+
+
+const { today: {low: lowToday, high: highToday}} = LOCAL_FORECAST;
+
