3e1207af5ce0cba2f3fd6475252d5313458411df
listopadgeorgii <listopadiogl@gmail.com>
2022-11-30 02:43:24 +0300

    Use getters and setters to Control Access to an Object

--- ES6.js
+++ ES6.js
@@ -18,3 +18,17 @@ const LOCAL_FORECAST = {
 
 const { today: {low: lowToday, high: highToday}} = LOCAL_FORECAST;
 
+class Thermostat {
+    constructor(fahrenheit) {
+        this.fahrenheit = fahrenheit;
+    }
+
+    get temperature() {
+        return (5 / 9) * (this.fahrenheit - 32);
+    }
+
+    set temperature(celsius) {
+        this.fahrenheit = (celsius * 9.0) / 5 + 32;
+    }
+}
+
