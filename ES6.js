287fe16932011bcace781dc87e0d77fe761ac7ad
listopadgeorgii <listopadiogl@gmail.com>
2022-11-30 03:04:11 +0300

    export||import

--- ES6.js
+++ ES6.js
@@ -32,3 +32,13 @@ class Thermostat {
     }
 }
 
+const uppercaseString = (string) => {
+    return string.toUpperCase();
+}
+
+const lowercaseString = (string) => {
+    return string.toLowerCase()
+}
+
+export { uppercaseString , lowercaseString};
+
