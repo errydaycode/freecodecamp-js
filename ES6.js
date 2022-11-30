119e2bd006966a1de4af0efbea76d47119272943
listopadgeorgii <listopadiogl@gmail.com>
2022-11-30 13:38:11 +0300

    then||catch

--- ES6.js
+++ ES6.js
@@ -63,3 +63,18 @@ const makeServerRequest = new Promise((resolve, reject) => {
         reject("Data not received");
     }
 });
+
+const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer is set to true to represent a successful response from a server
+    let responseFromServer = true;
+
+    if(responseFromServer) {
+        resolve("We got the data").then(result => {
+            console.log(result);
+        });
+    } else {
+        reject("Data not received").catch(error => {
+            concole.log(error);
+        });
+    }
+});
