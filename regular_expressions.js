bdcf28ad65ce454ca1c3b8f2e89659c3587a09d4
listopadgeorgii <listopadiogl@gmail.com>
2022-11-30 14:26:52 +0300

    ignore case while matching

--- regular_expressions.js
+++ regular_expressions.js
@@ -2,6 +2,12 @@
 // let myRegex = /Hello/;
 // let result = myRegex.test(myString);
 
-let petString = "James has a pet cat.";
-let petRegex = /dog|cat|bird|fish/; // Change this line
-let result = petRegex.test(petString);
+// let petString = "James has a pet cat.";
+// let petRegex = /dog|cat|bird|fish/; // Change this line
+// let result = petRegex.test(petString);
+
+let myString = "freeCodeCamp";
+let fccRegex = /freecodecamp/i; // Change this line
+let result = fccRegex.test(myString);
+
+console.log(result)
