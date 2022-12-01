0c30452d2be3b4775f93a4776482e32d8ca3c493
listopadgeorgii <listopadiogl@gmail.com>
2022-12-02 02:40:16 +0300

    Check for All or None

--- regular_expressions.js
+++ regular_expressions.js
@@ -1,23 +1,23 @@
 // let myString = "Hello, World!";
 // let myRegex = /Hello/;
 // let result = myRegex.test(myString);
-
+//
 // let petString = "James has a pet cat.";
 // let petRegex = /dog|cat|bird|fish/; // Change this line
 // let result = petRegex.test(petString);
-
+//
 // let myString = "freeCodeCamp";
 // // let fccRegex = /freecodecamp/i; // Change this line
 // // let result = fccRegex.test(myString);
 // //
 // // console.log(result)
-
+//
 // let humStr = "I'll hum a song";
 // let hugStr = "Bear hug";
 // let huRegex = /hu./;
 // huRegex.test(humStr);
 // huRegex.test(hugStr);
-
+//
 // let bigStr = "big";
 // let bagStr = "bag";
 // let bugStr = "bug";
@@ -27,7 +27,7 @@
 // bagStr.match(bgRegex);
 // bugStr.match(bgRegex);
 // bogStr.match(bgRegex);
-
+//
 //
 // let caboose = "The last car on a train is the caboose";
 // let lastRegex = /caboose$/;
@@ -36,7 +36,7 @@
 // let rickyAndCal = "Cal and Ricky both like racing.";
 // let calRegex = /^Cal/;
 // let result = calRegex.test(rickyAndCal);
-
+//
 // let quoteSample = "The five boxing wizards jump quickly.";
 // let alphabetRegexV2 = /\w/g; // Change this line
 // let result = quoteSample.match(alphabetRegexV2).length;
@@ -44,7 +44,13 @@
 // let quoteSample = "The five boxing wizards jump quickly.";
 // let nonAlphabetRegex = /\W/g;
 // let result = quoteSample.match(nonAlphabetRegex).length;
+//
+// let username = "JackOfAllTrades";
+// let userCheck = /^[a-z]([a-z]+\d*|[\d][\d])$/ig;
+// let result = userCheck.test(username);
 
-let username = "JackOfAllTrades";
-let userCheck = /^[a-z]([a-z]+\d*|[\d][\d])$/ig;
-let result = userCheck.test(username);
+let american = "color";
+let british = "colour";
+let rainbowRegex= /colou?r/;
+rainbowRegex.test(american);
+rainbowRegex.test(british);
