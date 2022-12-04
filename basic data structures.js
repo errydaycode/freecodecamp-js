e52546992c686207c9ec919c73b74fcc21f811cf
listopadgeorgii <listopadiogl@gmail.com>
2022-12-04 20:56:22 +0300

    pop|unshift

--- basic data structures.js
+++ basic data structures.js
@@ -23,3 +23,11 @@ function mixedNumbers(arr) {
 }
 
 console.log(mixedNumbers(['IV', 5, 'six']));
+
+function popShift(arr) {
+    let popped = arr.pop(); // Change this line
+    let shifted = arr.shift(); // Change this line
+    return [shifted, popped];
+}
+
+console.log(popShift(['challenge', 'is', 'not', 'complete']));
