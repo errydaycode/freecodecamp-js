253a344eb8f7d108a216b16dd569521e3e2d85f6
listopadgeorgii <listopadiogl@gmail.com>
2022-12-04 21:17:07 +0300

    add items using splice()

--- basic data structures.js
+++ basic data structures.js
@@ -4,35 +4,42 @@
 // let ourArray = ["a", "b", "c"];
 // let ourVariable = ourArray[0];
 // ourArray[1] = "not b anymore";
+//
+//
+// function mixedNumbers(arr) {
+//     arr.unshift('I', 2, 'three');
+//     arr.push(7,'VIII', 'nine');
+//
+//
+//     return arr;
+// }
+//
+// function mixedNumbers(arr) {
+//     // Only change code below this line
+//     arr.unshift('I', 2 , 'three');
+//     arr.push(7, 'VIII', 9);
+//     // Only change code above this line
+//     return arr;
+// }
+//
+// console.log(mixedNumbers(['IV', 5, 'six']));
+//
+// function popShift(arr) {
+//     let popped = arr.pop(); // Change this line
+//     let shifted = arr.shift(); // Change this line
+//     return [shifted, popped];
+// }
+//
+// console.log(popShift(['challenge', 'is', 'not', 'complete']));
+//
+// const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// arr.splice(0,1);
+// arr.splice(3,4);
+// console.log(arr);
 
+const numbers = [10, 11, 12, 12, 15];
+const startIndex = 3;
+const amountToDelete = 2;
 
-function mixedNumbers(arr) {
-    arr.unshift('I', 2, 'three');
-    arr.push(7,'VIII', 'nine');
-
-
-    return arr;
-}
-
-function mixedNumbers(arr) {
-    // Only change code below this line
-    arr.unshift('I', 2 , 'three');
-    arr.push(7, 'VIII', 9);
-    // Only change code above this line
-    return arr;
-}
-
-console.log(mixedNumbers(['IV', 5, 'six']));
-
-function popShift(arr) {
-    let popped = arr.pop(); // Change this line
-    let shifted = arr.shift(); // Change this line
-    return [shifted, popped];
-}
-
-console.log(popShift(['challenge', 'is', 'not', 'complete']));
-
-const arr = [2, 4, 5, 1, 7, 5, 2, 1];
-arr.splice(0,1);
-arr.splice(3,4);
-console.log(arr);
+numbers.splice(startIndex, amountToDelete, 13, 14, 15, 16, 17, 18, 19, 20, 'etc');
+console.log(numbers);
