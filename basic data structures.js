1c625cd9e17411a28708b6b1b08f8e6a4ce080be
listopadgeorgii <listopadiogl@gmail.com>
2022-12-04 20:48:38 +0300

    add items to an array with push() and unshift()

--- basic data structures.js
+++ basic data structures.js
@@ -1,6 +1,25 @@
 // let simpleArray = ['one', 2, 'three', true, false, undefined, null];
 // console.log(simpleArray.length);
 
-let ourArray = ["a", "b", "c"];
-let ourVariable = ourArray[0];
-ourArray[1] = "not b anymore";
+// let ourArray = ["a", "b", "c"];
+// let ourVariable = ourArray[0];
+// ourArray[1] = "not b anymore";
+
+
+function mixedNumbers(arr) {
+    arr.unshift('I', 2, 'three');
+    arr.push(7,'VIII', 'nine');
+
+
+    return arr;
+}
+
+function mixedNumbers(arr) {
+    // Only change code below this line
+    arr.unshift('I', 2 , 'three');
+    arr.push(7, 'VIII', 9);
+    // Only change code above this line
+    return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));
