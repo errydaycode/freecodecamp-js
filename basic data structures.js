e701b57512d0b97999225356d2504b6d7ebab771
listopadgeorgii <listopadiogl@gmail.com>
2022-12-04 20:27:43 +0300

    access an array's contents using bracket notation

--- basic data structures.js
+++ basic data structures.js
@@ -1,2 +1,6 @@
-let simpleArray = ['one', 2, 'three', true, false, undefined, null];
-console.log(simpleArray.length);
+// let simpleArray = ['one', 2, 'three', true, false, undefined, null];
+// console.log(simpleArray.length);
+
+let ourArray = ["a", "b", "c"];
+let ourVariable = ourArray[0];
+ourArray[1] = "not b anymore";
