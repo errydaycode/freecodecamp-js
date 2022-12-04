b3cc321d8ef7da85186080883e4d56bfaec3efd2
listopadgeorgii <listopadiogl@gmail.com>
2022-12-04 22:20:14 +0300

    copy an array with spread operator

--- basic data structures.js
+++ basic data structures.js
@@ -50,3 +50,16 @@ function forecast(arr) {
     return weather;
 }
 console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+function copyMachine(arr, num) {
+    let newArr = [];
+    while (num >= 1) {
+        // Only change code below this line
+        newArr.push([...arr])
+        // Only change code above this line
+        num--;
+    }
+    return newArr;
+}
+
+console.log(copyMachine([true, false, true], 2));
