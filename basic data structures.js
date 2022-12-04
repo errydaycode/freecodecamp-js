132731e5cea8651b31ca8f6540936ed5f50c1af2
listopadgeorgii <listopadiogl@gmail.com>
2022-12-04 21:09:16 +0300

    remove items using splice()

--- basic data structures.js
+++ basic data structures.js
@@ -31,3 +31,8 @@ function popShift(arr) {
 }
 
 console.log(popShift(['challenge', 'is', 'not', 'complete']));
+
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+arr.splice(0,1);
+arr.splice(3,4);
+console.log(arr);
