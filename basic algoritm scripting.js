9b339fb1d4cd767c0f50a06f8308e09ffffb940c
listopadgeorgii <listopadiogl@gmail.com>
2022-12-06 23:47:56 +0300

    8kuy codewars loops

--- basic algoritm scripting.js
+++ basic algoritm scripting.js
@@ -56,3 +56,14 @@ function findShort(s){
     }
     return smallest.length;
 }
+
+function grabDoll(dolls){
+    var bag=[];
+    for (let i = 0; i < dolls.length; i++) {
+        if(dolls[i] === "Hello Kitty" || dolls[i] === "Barbie doll") {
+            bag.push(dolls[i])
+        }else continue;
+        if (bag.length === 3) break;
+    }
+    return bag;
+}
