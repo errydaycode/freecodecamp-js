092ca03a6c94fae225faee04e67d27238e59782f
listopadgeorgii <listopadiogl@gmail.com>
2022-12-06 19:38:54 +0300

    find longest word

--- basic algoritm scripting.js
+++ basic algoritm scripting.js
@@ -29,3 +29,18 @@ function getCount(str) {
     let result = str.match(vowelRegex);
     return result?.length || 0 ;
 };
+
+// Find longest word
+
+function findLongestWordLength(str) {
+    let longestWord = "";
+    let words = str.split(" ")
+    for (let i = 0; i < words.length; i++) {
+        if(words[i].length > longestWord.length) {
+            longestWord = words[i];
+        }
+    }
+    return longestWord.length;
+}
+
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
