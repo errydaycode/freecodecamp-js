339b4ec55dc1f7c08ac5a5bdab4d02d9bb437dc7
listopadgeorgii <listopadiogl@gmail.com>
2022-12-06 13:03:21 +0300

    reverse a string

--- basic algoritm scripting.js
+++ basic algoritm scripting.js
@@ -1,6 +1,13 @@
+
+// Convert from celsius to fahrenheit
 function convertCtoF(celsius) {
     let fahrenheit = celsius * 9/5 + 32;
     return fahrenheit;
 }
 
 convertCtoF(30);
+
+// Reverse a string
+function solution(str){
+    return str.split("").reverse().join("")
+}
