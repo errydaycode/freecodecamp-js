103be304985d52f595ea130af454e7f7ac163d19
listopadgeorgii <listopadiogl@gmail.com>
2022-12-09 01:02:57 +0300

    slice

--- basic algoritm scripting.js
+++ basic algoritm scripting.js
@@ -140,3 +140,9 @@ function titleCase(str) {
 }
 
 console.log(titleCase("I'm a little tea pot"));
+
+// Slice & splice
+function frankenSplice(arr1, arr2, n) {
+    return [...arr2.slice(0, n), ...arr1, ...arr2.slice(n)];
+}
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
