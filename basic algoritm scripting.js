dd2c96f3ba5fca8d90d436e1a39eba8363f75c6e
listopadgeorgii <listopadiogl@gmail.com>
2022-12-10 21:09:03 +0300

    sum all numbers in a range

--- basic algoritm scripting.js
+++ basic algoritm scripting.js
@@ -194,3 +194,15 @@ function whatNumberIsIt(n){
     }
 }
 
+
+function sumAll(arr) {
+    let minimumNumber = Math.min(arr[0], arr[1]);
+    let maximumNumber = Math.max(arr[0], arr[1]);
+    var result = 0;
+    for (var i = minimumNumber; i <= maximumNumber; i += 1) {
+        result += i;
+    }
+    return result;
+}
+
+sumAll([1, 4]);
