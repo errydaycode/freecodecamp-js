ec1029cb8fd95564216310933e10479eae8d8782
listopadgeorgii <listopadiogl@gmail.com>
2022-12-11 15:28:15 +0300

    split and join

--- codewars.js
+++ codewars.js
@@ -1,2 +1,4 @@
 // 7 kyu codewars
 const squareDigits = num => +String(num).split('').map(e => (+e) ** 2).join('');
+// 8 kyu codewars
+const splitAndMerge =(string, separator) => string.split(' ').map(word => word.split('').join(separator)).join(' ')
