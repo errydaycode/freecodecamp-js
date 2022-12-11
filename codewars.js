56ddcc139d1ad05f657d32f16fc9f3803e530083
listopadgeorgii <listopadiogl@gmail.com>
2022-12-12 02:47:06 +0300

    basic calculator

--- codewars.js
+++ codewars.js
@@ -2,3 +2,24 @@
 const squareDigits = num => +String(num).split('').map(e => (+e) ** 2).join('');
 // 8 kyu codewars
 const splitAndMerge =(string, separator) => string.split(' ').map(word => word.split('').join(separator)).join(' ')
+// 7 kyu codewars
+function calculate(num1, operation, num2) {
+    let result = null;
+    switch (operation) {
+        case "+" :
+            result =   num1 + num2;
+            break;
+        case "-" :
+            result = num1 - num2;
+            break;
+        case "*" :
+            result = num1 * num2;
+            break;
+        case "/" :
+            result = num1 / num2;
+            break;
+
+    }
+    if (result === -Infinity) return null;
+    return result
+}
