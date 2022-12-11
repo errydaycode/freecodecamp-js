45be9a8491cd737833c711ac613fb532c05e40ec
listopadgeorgii <listopadiogl@gmail.com>
2022-12-11 16:20:41 +0300

    for each

---
+++ js_methods.js
@@ -0,0 +1,19 @@
+const people = [
+    {name: "Иван" , age: 26, budget : 35000},
+    {name: "Георгий" , age: 26, budget : 14400},
+    {name: "Олеся" , age: 34, budget : 6500},
+    {name: "Катя" , age: 76, budget : 13500},
+    {name: "Влад" , age: 5, budget : 9100},
+    {name: "Соня" , age: 17, budget : 3800}
+]
+
+// for (let i = 0; i < people.length; i++) {
+//   console.log(people[i])
+// }
+
+// for (let person of people) {
+//   console.log(person)
+// }
+
+// For Each
+// people.forEach(person => console.log(person));
