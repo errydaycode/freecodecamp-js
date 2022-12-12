abce6cdd207a76583959f51654f68bdedd0b5ad2
listopadgeorgii <listopadiogl@gmail.com>
2022-12-12 17:50:47 +0300

    instanceof

--- basics_of_oop.js
+++ basics_of_oop.js
@@ -54,3 +54,11 @@ function Dog(name, color) {
 let terrier = new Dog("Alfa", "Red");
 
 // Verify an Object's Constructor with instanceof
+
+function House(numBedrooms) {
+    this.numBedrooms = numBedrooms;
+}
+
+let myHouse = new House (1);
+
+myHouse instanceof House
