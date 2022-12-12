ce8fc7881ac3dfd7092d2067f0a080063710af1f
listopadgeorgii <listopadiogl@gmail.com>
2022-12-12 16:10:39 +0300

    oop basics

---
+++ basics_of_oop.js
@@ -0,0 +1,26 @@
+// Create a Basic JavaScript Object
+let dog = {
+    name : "Richi",
+    numLegs: 4
+};
+
+// Use Dot Notation to Access the Properties of an Object
+let dog = {
+    name: "Spot",
+    numLegs: 4
+};
+
+console.log(dog.name);
+console.log(dog.numLegs)
+
+// Create a Method on an Object
+
+let dog = {
+    name: "Spot",
+    numLegs: 4,
+    sayLegs : function() {
+        return "This dog has " + dog.numLegs + " legs."
+    }
+};
+
+dog.sayLegs();
