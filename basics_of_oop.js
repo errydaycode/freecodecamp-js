8db285b862fac6357c7866a403ab2589a2e1b683
listopadgeorgii <listopadiogl@gmail.com>
2022-12-13 00:35:15 +0300

    prototype

--- basics_of_oop.js
+++ basics_of_oop.js
@@ -76,3 +76,12 @@ for (let property in canary) {
         ownProps.push(property);
     }
 }
+
+// Use Prototype Properties to Reduce Duplicate Code
+
+function Dog(name) {
+    this.name = name;
+}
+Dog.prototype.numLegs = 4;
+
+let beagle = new Dog("Snoopy");
